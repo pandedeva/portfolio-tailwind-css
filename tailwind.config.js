afe46f37497aa0fd3158c875ce984560b1e4cd41
deva <devapande66@gmail.com>
2022-11-04 22:53:54 +0800

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "40px",
+    },
+    extend: {
+      colors: {
+        primary: "#533E85",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
